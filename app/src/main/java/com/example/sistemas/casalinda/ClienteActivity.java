5413ead64c3511f78bdefbbfa62f2c52c4d404a1
Orlando Zambrano <ozambrano55@gmail.com>
2020-02-04 13:04:01 -0500

    actualiza a 04/02/2020 13:03 guardaCliente

--- app/src/main/java/com/example/sistemas/casalinda/ClienteActivity.java
+++ app/src/main/java/com/example/sistemas/casalinda/ClienteActivity.java
@@ -4,10 +4,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,8 +18,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.sistemas.casalinda.Utilidades.claseGlobal;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -33,7 +37,10 @@ public class ClienteActivity extends AppCompatActivity {
     ArrayList<Ciudad>ciudadList;
     ArrayList<String>listaCiudad;
     Connection connect;
-    String country;
+    String country,city;
+   // String cedula,nombre,apellido,telefono,correo,direccion;
+    Button nuevo,guarda, editar, salir;
+
     private boolean isFirstTime=true;
     public String tp;
     @Override
@@ -42,22 +49,25 @@ public class ClienteActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_cliente);
 
-        Button nuevo = findViewById(R.id.btnNuevo);
-        Spinner tipo = findViewById(R.id.spTipo);
-        Spinner pais = findViewById(R.id.spPais);
-        Spinner provincia = findViewById(R.id.spProvincia);
-        Spinner ciudad = findViewById(R.id.spCiudad);
+        salir=findViewById(R.id.btnSalir);
+        nuevo = findViewById(R.id.btnNuevo);
+        guarda=findViewById(R.id.btnGuardar);
+        editar=findViewById(R.id.btnEditar);
+        final Spinner tipo = findViewById(R.id.spTipo);
+        final Spinner pais = findViewById(R.id.spPais);
+        final Spinner provincia = findViewById(R.id.spProvincia);
+        final Spinner ciudad = findViewById(R.id.spCiudad);
 
 
-        TextInputEditText cedula = findViewById(R.id.tiedtCedula);
+        EditText cedula = findViewById(R.id.tiedtCedula);
         TextView vcedula = findViewById(R.id.txtCedula);
-        TextInputEditText nombre = findViewById(R.id.tiedtNombre);
-        TextInputEditText apellido = findViewById(R.id.tiedtApellido);
-        TextInputEditText telefono = findViewById(R.id.tiedtTelefono);
-        TextInputEditText correo = findViewById(R.id.tiedtCorreo);
-        TextInputEditText direccion = findViewById(R.id.tiedtDireccion);
+        EditText nombre = findViewById(R.id.tiedtNombre);
+        EditText apellido = findViewById(R.id.tiedtApellido);
+        EditText telefono = findViewById(R.id.tiedtTelefono);
+        EditText correo = findViewById(R.id.tiedtCorreo);
+        EditText direccion = findViewById(R.id.tiedtDireccion);
 
-
+        tipo.setEnabled(false);
         cedula.setEnabled(false);
         nombre.setEnabled(false);
         apellido.setEnabled(false);
@@ -67,6 +77,8 @@ public class ClienteActivity extends AppCompatActivity {
         pais.setEnabled(false);
         provincia.setEnabled(false);
         ciudad.setEnabled(false);
+        editar.setEnabled(false);
+        guarda.setEnabled(false);
 
 
 /*
@@ -81,10 +93,54 @@ public class ClienteActivity extends AppCompatActivity {
             provincia.setEnabled(false);
             ciudad.setEnabled(false);
 */
+        nuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tipo.setEnabled(true);
+                cargaTipo();
+                ArrayAdapter<CharSequence> adaptadorTipo = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listaTipo);
+                tipo.setAdapter(adaptadorTipo);
+                nuevo.setEnabled(false);
+                guarda.setEnabled(true);
+            }
+        });
+        guarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        cargaTipo();
-        ArrayAdapter<CharSequence> adaptadorTipo = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listaTipo);
-        tipo.setAdapter(adaptadorTipo);
+                String ce,no,ap,di,te,co;
+                ce=cedula.getText().toString();
+                no=nombre.getText().toString();
+                ap=apellido.getText().toString();
+                di=direccion.getText().toString();
+                te=telefono.getText().toString();
+                co=correo.getText().toString();
+              try {
+                  switch (tp) {
+                      case "CI":
+                          if (vcedula.getText().equals("Cédula Correcta")) {
+                              grabaCliente(ce,no,ap,tp,di,city,te,co);
+                          } else {
+                              salirp("Documento Identidad", "Digite Cédula válida", 1, 1);
+                          }
+                          break;
+                      case "CE":
+                          //grabaCliente();
+                          break;
+                      case "RUC":
+                          //grabaCliente();
+                          break;
+                  }
+              }catch (Exception e){salir(e.getMessage());}
+
+            }
+        });
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salirp("Ficha Cliente","Desea Salir",2,1);
+            }
+        });
 
         tipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -92,9 +148,12 @@ public class ClienteActivity extends AppCompatActivity {
                 if (isFirstTime)
                 {isFirstTime=false;}
                 else {
-                    country = tipoList.get(position).getC_tipo_identi();
+                    tp = tipoList.get(position).getC_tipo_identi();
                     activarCampos();
                     tipo.setEnabled(false);
+                    cargaPais();
+                    ArrayAdapter<CharSequence>adaptadorPais=new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,paisList);
+                    pais.setAdapter(adaptadorPais);
                 }
             }
 
@@ -103,11 +162,6 @@ public class ClienteActivity extends AppCompatActivity {
 
             }
         });
-
-        cargaPais();
-        ArrayAdapter<CharSequence>adaptadorPais=new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,paisList);
-        pais.setAdapter(adaptadorPais);
-
         cedula.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -158,9 +212,95 @@ public class ClienteActivity extends AppCompatActivity {
 
             }
         });
+        ciudad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                city=ciudadList.get(position).getC_ciudad();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
-private void activarCampos(){
+    private void grabaCliente(String ce, String no, String ap, String t, String di, String c, String te, String co) {
+        String ConnectionResult = "";
+        claseGlobal objEscritura=(claseGlobal)getApplicationContext();
+        claseGlobal objLectura=(claseGlobal)getApplicationContext();
+
+
+
+        try {
+            ConnectionStr conStr = new ConnectionStr();
+            connect = conStr.connectionclasss();
+            if (connect == null) {
+                ConnectionResult = "Revisar tu conexion a internet!";
+                Toast.makeText(getApplicationContext(), ConnectionResult, Toast.LENGTH_LONG).show();
+            } else {
+                CallableStatement call = connect.prepareCall("{call sp_insertCliente (?,?,?,?,?,?,?,?,?,?)}");
+                call.setString(1,ce);//C_Cod_Cliente
+                call.setString(2,no);//D_Nom_Cliente
+                call.setString(3,ap);//D_Ape_Cliente
+                call.setString(4,t);//C_Tipo_Identi
+                call.setString(5,ce);//Num_Identi
+                call.setString(6,di);//Direccion_Clien
+                call.setString(7,c);//C_Ciudad
+                call.setString(8,te);//Tel1
+                call.setString(9,co);//Contacto_mail
+                call.setString(10,objLectura.getC_funcionario());//C_Funcionario
+                call.execute();
+
+            }
+        }catch (Exception e){salirp("Guarda Cliente",e.getMessage(),1,1);}
+
+    }
+
+    //Validar atras
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+
+
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            // Esto es lo que hace mi botón al pulsar ir a atrás
+
+            //Toast.makeText(getApplicationContext(), "Voy hacia atrás!!",     Toast.LENGTH_SHORT).show();
+            //onBackPressed();
+
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("Quiere salir de la ficha cliente");
+            builder.setTitle("Confirmación");
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+            builder.setCancelable(false)
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            onBackPressed();
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+            builder.create();
+            builder.show();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void activarCampos(){
     TextInputEditText cedula=findViewById(R.id.tiedtCedula);
     TextView vcedula=findViewById(R.id.txtCedula);
     TextInputEditText nombre=findViewById(R.id.tiedtNombre);
@@ -179,10 +319,11 @@ private void activarCampos(){
     telefono.setEnabled(true);
     correo.setEnabled(true);
     direccion.setEnabled(true);
-    pais.setEnabled(true);
+   // pais.setEnabled(true);
     provincia.setEnabled(true);
     ciudad.setEnabled(true);
 }
+
 
     //CargaTipo
 
@@ -226,7 +367,7 @@ private void activarCampos(){
             ConnectionStr conStr = new ConnectionStr();
             connect = conStr.connectionclasss();        // Connect to database
 
-            String query = "select (rtrim(C_Pais)+'-'+ rtrim(N_Pais_En)) as Pais from Gen_Paises";
+            String query = "select (rtrim(C_Pais)+'-'+ rtrim(N_Pais_En)) as Pais from Gen_Paises where c_pais='ec'";
             Statement stmt = connect.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             if (rs!=null) {
@@ -328,6 +469,49 @@ private void activarCampos(){
                 });
         AlertDialog titulo=alerta.create();
         titulo.setTitle("Salida");
+        titulo.show();
+        //finish();
+        //super.onBackPressed();
+
+    }
+    public void salirp(String t,String m,int a, int b){
+        LinearLayout linear = findViewById(R.id.linear_layout);
+
+        AlertDialog.Builder alerta=new AlertDialog.Builder(this);
+        alerta.setMessage(m)
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       switch (a) {
+                           case 1:
+                               dialog.cancel();
+                               break;
+                           case 2:
+                                finish();
+                               break;
+
+
+                       }
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (b) {
+                            case 1:
+                                dialog.cancel();
+                                break;
+                            case 2:
+                                finish();
+                                break;
+
+
+                        }
+                    }
+                });
+        AlertDialog titulo=alerta.create();
+        titulo.setTitle(t);
         titulo.show();
         //finish();
         //super.onBackPressed();
