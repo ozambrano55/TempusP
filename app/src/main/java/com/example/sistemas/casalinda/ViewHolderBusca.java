d4a95ebfa0fbfcab16a8a9cdb55b20f837dce146
Orlando Zambrano <ozambrano55@gmail.com>
2020-02-07 16:59:41 -0500

    07/02/2020

--- app/src/main/java/com/example/sistemas/casalinda/ViewHolderBusca.java
+++ app/src/main/java/com/example/sistemas/casalinda/ViewHolderBusca.java
@@ -1,6 +1,15 @@
 package com.example.sistemas.casalinda;
 
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ViewHolderBusca extends RecyclerView.ViewHolder {
+    private TextView codigo, nombre;
+
+    public ViewHolderBusca( View iteemView){
+        super(iteemView);
+        codigo=iteemView.findViewById(R.id.txtCodigo);
+    }
 }
